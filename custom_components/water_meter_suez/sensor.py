8d39cde6fdf5827485af6e8b0190fcd158fa9382
Dan Monceau <dan.monceau.lpcb@gmail.com>
2024-06-07 14:12:51 +0200

    Update sensor.py

--- custom_components/water_meter_suez/sensor.py
+++ custom_components/water_meter_suez/sensor.py
@@ -19,4 +19,18 @@ class WaterMeterSensor(CoordinatorEntity, SensorEntity):
         """Initialize the sensor."""
         super().__init__(coordinator)
         self._attr_name = "Water Meter Consumption"
-        self._attr_unique_id = "water
+        self._attr_unique_id = "water_meter_consumption"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        # Return the latest total volume
+        return self.coordinator.data["total_volumes"][-1]
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        # Return all total volumes as an attribute
+        return {
+            "total_volumes": self.coordinator.data["total_volumes"]
+        }
