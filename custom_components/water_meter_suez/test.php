c2d4f1beb7669ec3257810f3af6300377c1a0385
Dan Monceau <dan@eisge.com>
2024-09-23 09:05:41 +0200

    S 9 - gfhgfhgfhggh

--- custom_components/water_meter_suez/test.php
+++ custom_components/water_meter_suez/test.php
@@ -1,3 +1,37 @@
+<?php
+/**
+ * Scenario file  
+ * 
+ * PHP version 5
+ * 
+ * @category Patch
+ * 
+ * @package Backend
+ * 
+ * @author EISGE <kalifast@eisge.com>
+ * 
+ * @license kalifast.com Kalifast
+ * 
+ * @version SVN: $Id$
+ * 
+ * @link kalifast.com
+ */ 
+
+/**
+ * Patch 
+ * 
+ * @category Patch
+ * 
+ * @package Backend
+ * 
+ * @author EISGE <kalifast@eisge.com>
+ * 
+ * @license kalifast.com Kalifast
+ * 
+ * @link kalifast.com
+ */
+class Patch extends BaseApi
+{
 /**
      * Récupération des logs du backup
      * 
@@ -18,3 +52,26 @@
 
         $this->setData($content);
     }
+
+    /**
+     * Récupération des logs du backup
+     * 
+     * @return string
+     */
+    function getBackupLvsfdsffdssdfogs()
+    {
+        $d = $this->checkParams(
+            [
+                'backup_name' => 'string'
+            ]
+        );
+
+        $backup_name = preg_replace('/\\.[^.\\s]{2,4}$/', '', preg_replace('/\\.[^.\\s]{2,4}$/', '', $d->backup_name));
+
+        $file = '../../backup/'.$backup_name.'_error.log';
+        $content = file_get_contents($file);
+
+        $this->setData($content);
+    }
+
+}
